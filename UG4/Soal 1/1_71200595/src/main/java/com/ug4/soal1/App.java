d539495dabd1fb470f6e4b082fb0389dc55b2891
Gerrhut <78344798+Gerrhut@users.noreply.github.com>
2022-03-02 17:58:34 +0700

    Add files via upload

---
+++ UG4/Soal 1/1_71200595/src/main/java/com/ug4/soal1/App.java
@@ -0,0 +1,33 @@
+package com.ug4.soal1;
+import javax.xml.bind.annotation.XmlEnum;
+import java.util.Scanner;
+
+import java.sql.Driver;
+import java.util.ArrayList;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    String name;
+    Driver driver;
+    Integer Capacity;
+    Integer usedCapacity;
+    Integer age;
+    String gender;
+    String email;
+    String destiny;
+    double profit;
+    double balance;
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    //String Menu(Passenger passenger, Bus bus){
+    //    return name;
+
+}
---
+++ UG4/Soal 1/1_71200595/src/main/java/com/ug4/soal1/Bus.java
@@ -0,0 +1,18 @@
+package com.ug4.soal1;
+
+import java.util.ArrayList;
+
+public class Bus {
+    String name;
+    Driver driver;
+    Integer Capacity;
+    Integer usedCapacity;
+    double profit;
+    String Route;
+    final double fares;
+    final ArrayList <Passenger>;
+
+    public Driver getDriver() {
+        return driver;
+    }
+}
