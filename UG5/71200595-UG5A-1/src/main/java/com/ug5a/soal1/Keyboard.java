608f18ba77ef006ff458525adf98195fceb598f5
Gerrhut <78344798+Gerrhut@users.noreply.github.com>
2022-03-09 18:56:20 +0700

    Add files via upload

---
+++ UG5/71200595-UG5A-1/src/main/java/com/ug5a/soal1/Keyboard.java
@@ -0,0 +1,20 @@
+package com.ug5a.soal1;
+import java.util.Scanner;
+import javax.naming.NameAlreadyBoundException;
+
+public class Keyboard {
+    String merkModel;
+    long harga;
+
+    public static void setMerkModel(String merkModel){
+
+    }
+
+    public static void setHarga(long Harga){
+
+    }
+
+    String getMerkModel(){return merkModel;}
+
+    long getHarga(){setHarga();}
+}
---
+++ UG5/71200595-UG5A-1/src/main/java/com/ug5a/soal1/TransPay.java
@@ -0,0 +1,24 @@
+package com.ug5a.soal1;
+import java.util.Scanner;
+
+public class TransPay {
+    String nama;
+    long saldo;
+
+    String getNama(){return nama;}
+
+    long getSaldo(){return saldo;}
+
+    public static void setNama(String nama){
+        System.out.println (nama);}
+
+    public static void setSaldo(long saldo){
+        System.out.println(saldo);}
+
+    void topUp(long topUp){
+
+    }
+
+    void bayar(int){
+        int getSaldo();}
+}
